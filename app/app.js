f42c44503112bd986f5cc46a5298c2db01196d78
Silvio Netto <silvio.netto@gmail.com>
2021-12-30 00:28:46 +0100

    Agora vaigit add .!

--- app/app.js
+++ app/app.js
@@ -1,19 +1,13 @@
 // Configure Web3
 const Web3 = require('web3')
 
-const env = {
-  "ETH_ACCOUNT": "${{ secrets.ETH_ACCOUNT }}",
-  "ETH_PRIVATE_KEY": "${{ secrets.ETH_PRIVATE_KEY }}",
-  "INFURA_API_KEY": "${{ secrets.INFURA_API_KEY }}"
-}
-
-const INFURA_URL = 'https://rinkeby.infura.io/v3/' + env.INFURA_API_KEY
+const INFURA_URL = 'https://rinkeby.infura.io/v3/' + process.env.INFURA_API_KEY
 
 const web3 = new Web3(INFURA_URL)
 
 // const web3 = new Web3('https://rinkeby.infura.io/v3/INFURA_API_KEY')
-const account = env.ETH_ACCOUNT
-web3.eth.accounts.wallet.add(env.ETH_PRIVATE_KEY)
+const account = process.env.ETH_ACCOUNT
+web3.eth.accounts.wallet.add(process.env.ETH_PRIVATE_KEY)
 
 // Dai Token
 const daiAddress = '0xc7ad46e0b8a400bb3c915120d284aafba8fc4735'
